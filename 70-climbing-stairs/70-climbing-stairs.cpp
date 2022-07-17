351b682c728e4e9340812bed7e763b82dbac1024
Omar El-Hariry <48570458+OmarHariry@users.noreply.github.com>
2022-07-17 18:34:17 +0200

    Time: 0 ms (100.00%), Space: 5.9 MB (83.64%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,35 +1,18 @@
-#include<vector>
+vector<int>dp(46,-1);
 class Solution {
 public:
-    /*int climbStairs(int n) {
-    normal recursive 
-    Time O(2^N)
-    space O(n)
-		if (n <= 1) return 1;
-		if (n == 2)return 2;
-		return climbStairs(n - 1) + climbStairs(n - 2);
-	}*/
-    /*
-    int climbStairs(int n) {
-    vector<int>arr(n + 1);
-		if (n == 1)
-			return 1;
-		arr[0] = 0;
-		arr[1] = 1;
-		arr[2] = 2;
-		for (int i = 3; i <=n;++i) {
-			arr[i] = arr[i - 2] + arr[i - 1];
-
-		}
-		return arr.back();}*/
     
-	int climbStairs(int n) {
-		vector<int>dp(n + 1, -1);
-		dp[0] = 1;
-		dp[1] = 1;
-		for (int i = 2; i <= n; ++i) {
-			dp[i] = dp[i - 1] + dp[i - 2];
-		}
-		return dp[n];
-	}
+    
+    int climbStairs(int n) {
+        if(n == -1)
+            return 0;
+        
+        if(n == 0)
+            return 1;
+        
+        if(dp[n] != -1)
+            return dp[n];
+        
+        return dp[n] = climbStairs(n - 1) + climbStairs(n-2);
+    }
 };
