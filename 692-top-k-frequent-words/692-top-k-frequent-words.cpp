62fd2ac751c427e9bd89e47254a91b6d8f2562dd
Omar El-Hariry <48570458+OmarHariry@users.noreply.github.com>
2022-07-28 00:06:43 +0200

    Time: 17 ms (67.22%), Space: 12.7 MB (33.29%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -1,26 +1,28 @@
-bool comp(pair<string,int>&a,pair<string,int>&b){
+class comp{
+public:
+    bool operator()(pair<string,int>&a, pair<string,int>&b){
         if(a.second == b.second)
-            return a.first < b.first;
-        return a.second > b.second;
+            return a.first > b.first;
+        return a.second < b.second;
     }
+};
 class Solution {
 public:
-    
     vector<string> topKFrequent(vector<string>& words, int k) {
-        unordered_map<string,int>mp;
+        priority_queue<pair<string,int>, vector<pair<string,int>>, comp> maxHeap;
         
+        unordered_map<string,int>mp;
         for(string word : words)mp[word]++;
         
-        vector<pair<string,int>>arr;
         for(auto it : mp)
-            arr.push_back({it.first,it.second});
-        
-        sort(arr.begin(),arr.end(),comp);
+            maxHeap.push(it);
         
         vector<string>ans;
-        for(int i=0;i<k;++i){
-            ans.push_back(arr[i].first);
+        while(k--){
+            ans.push_back(maxHeap.top().first);
+            maxHeap.pop();
         }
+        
         return ans;
     }
 };
