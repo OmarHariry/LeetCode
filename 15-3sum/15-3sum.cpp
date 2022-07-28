130edd95385ae5710aaa17740aec549819c3cfdc
Omar El-Hariry <48570458+OmarHariry@users.noreply.github.com>
2022-07-29 01:53:59 +0200

    Time: 2207 ms (5.02%), Space: 295.6 MB (5.41%) - LeetHub

---
+++ 15-3sum/15-3sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        map<vector<int>, int>mp;
+        
+        
+        vector<vector<int>>ans;
+        for(int i=0;i<nums.size();++i){
+            int j = i+1,k= nums.size()-1;
+            
+            while(j < k){
+                if(nums[i] + nums[j] + nums[k] == 0){
+                    vector<int>triplet({nums[i],nums[j],nums[k]});
+                    if(mp.find(triplet) == mp.end()){
+                        mp[triplet]++;
+                        ans.push_back(triplet);
+                        
+                    }
+                    ++j;
+                }
+                else if(nums[i] + nums[j] + nums[k] < 0)
+                    ++j;
+                else
+                    --k;
+            }
+            
+        }
+        return ans;
+    }
+};
