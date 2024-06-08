97133cc61cd180f61bb8f46942b055e19c5318e4
Omar El-Hariry <48570458+OmarHariry@users.noreply.github.com>
2024-06-08 23:02:07 +0300

    Time: 43 ms (80.01%), Space: 48.1 MB (69.58%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -2,48 +2,46 @@ class TrieNode {
 public:
     TrieNode* children[26];
     bool isEndWord;
+    
     TrieNode() {
         isEndWord = false;
         for(auto &child : children)
-            child = NULL;
+            child = nullptr;
     }
 };
 class Trie {
-    TrieNode* root;
+TrieNode*root;
 public:
     Trie() {
         root = new TrieNode();
     }
     
     void insert(string word) {
-        TrieNode* curr = root;
-        
-        for(char c : word) {
-            if (!curr->children[c-'a'])
-                curr->children[c-'a'] = new TrieNode();
-            curr = curr->children[c-'a'];
+        TrieNode*curr = root;
+        for(int i=0;i<word.size();++i) {
+            if(!curr->children[word[i] - 'a'])
+                curr->children[word[i] - 'a'] = new TrieNode();
+            curr = curr->children[word[i] - 'a'];
         }
         curr->isEndWord = true;
     }
     
     bool search(string word) {
         TrieNode* curr = root;
-        
-        for(char c : word) {
-            if(!curr->children[c-'a'])
+        for(int i=0;i<word.size();++i) {
+            if(!curr->children[word[i] - 'a'])
                 return false;
-            curr = curr->children[c-'a'];
+            curr = curr->children[word[i] - 'a'];
         }
         return curr->isEndWord;
     }
     
     bool startsWith(string prefix) {
         TrieNode* curr = root;
-        
-        for(char c : prefix) {
-            if(!curr->children[c-'a'])
+        for(int i=0;i<prefix.size();++i) {
+            if(!curr->children[prefix[i] - 'a'])
                 return false;
-            curr = curr->children[c-'a'];
+            curr = curr->children[prefix[i] - 'a'];
         }
         return true;
     }
