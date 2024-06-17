0597cdb97ca16dad3e5c3ca8288952f27e416cb2
Omar El-Hariry <48570458+OmarHariry@users.noreply.github.com>
2024-06-17 16:07:59 +0300

    Time: 15 ms (59.45%), Space: 10.2 MB (68.70%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int>freq1(26,0);
+        vector<int>freq2(26,0);
+        
+        for(char c : ransomNote) freq1[c-'a']++;
+        for(char c : magazine) freq2[c-'a']++;
+        
+        for(int i=0;i<26;++i){
+            if(freq1[i] > freq2[i])
+                return false;
+        }
+        return true;
+    }
+};
