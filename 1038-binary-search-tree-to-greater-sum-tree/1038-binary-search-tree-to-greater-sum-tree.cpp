6404858aa6cf7042c466fdeda05ee6da0564bf52
Omar El-Hariry <48570458+OmarHariry@users.noreply.github.com>
2024-06-25 14:40:20 +0300

    Time: 0 ms (100.00%), Space: 10.1 MB (42.71%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -11,35 +11,18 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode*root, vector<int>& traversal) {
-        if(!root)
-            return;
-        inorder(root->left, traversal);
-        traversal.push_back(root->val);
-        inorder(root->right, traversal);
-    }
-    void replaceValues(TreeNode* root, vector<int>& traversal) {
-        if (!root) 
+    void inorder(TreeNode*root, int& sum) {
+        if (!root)
             return;
         
-        replaceValues(root->left, traversal);
-        
-        int sum = 0;
-        for(int i : traversal) {
-            if (i >= root->val)
-                sum += i;
-            else
-                break;
-        }
+        inorder(root->right, sum);
+        sum += root->val;
         root->val = sum;
-        replaceValues(root->right, traversal);
+        inorder(root->left, sum);
     }
     TreeNode* bstToGst(TreeNode* root) {
-        vector<int> traversal;
-        inorder(root, traversal);
-        reverse(traversal.begin(), traversal.end());
-        
-        replaceValues(root, traversal);
+        int sum = 0;
+        inorder(root, sum);
         return root;
     }
 };
