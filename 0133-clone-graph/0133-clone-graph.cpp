d95adf5b27b73a7562e929ecaff56634fe5386d1
Omar El-Hariry <48570458+OmarHariry@users.noreply.github.com>
2024-07-04 16:22:54 +0300

    Time: 2 ms (75.44%), Space: 11.6 MB (31.83%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -21,29 +21,31 @@ public:
 
 class Solution {
 public:
-    Node* dfs(Node* node, unordered_map<Node*, Node*>& mp){ 
+    Node* dfs(Node* node, unordered_map<Node*, Node*>& mp) {
         Node *clone = new Node(node->val);
         mp[node] = clone;
         vector<Node*> neighbors;
+        
         for(auto neighbor : node->neighbors) {
-            if (mp.find(neighbor) != mp.end()) 
+            if (mp.find(neighbor) != mp.end()) {
                 neighbors.push_back(mp[neighbor]);
-            else
+            } else {
                 neighbors.push_back(dfs(neighbor, mp));
+            }
         }
         clone->neighbors = neighbors;
         return clone;
     }
     Node* cloneGraph(Node* node) {
-        if (!node)
+        if(!node)
             return nullptr;
         
-        if (node->neighbors.size() == 0) {
+        if(node->neighbors.size() == 0) {
             Node*clone = new Node(node->val);
             return clone;
         }
         
-        unordered_map<Node*, Node*>mp;
+        unordered_map<Node*, Node*> mp;
         return dfs(node, mp);
     }
 };
