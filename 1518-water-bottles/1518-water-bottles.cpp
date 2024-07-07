9d0c3ca4f2768d02b4600749762356ba13797d33
Omar El-Hariry <48570458+OmarHariry@users.noreply.github.com>
2024-07-07 13:00:12 +0300

    Time: 0 ms (100.00%), Space: 7.2 MB (30.43%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.cpp
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        int Q = 0, R = 0, d = 0, e = 0, ans = 0;
-        while(numBottles) {
-            Q = numBottles / numExchange, R = numBottles % numExchange;
-            if (!Q && R) 
-                d = R;
-            else
-                d = Q * numExchange;
-            e = Q;
-            numBottles = numBottles - d + e;
-            ans += d;
+        int consumedBottles = 0;
+        while (numBottles >= numExchange) {
+            consumedBottles += numExchange;
+            numBottles -= numExchange;
+            numBottles++;
         }
-        return ans;
+        return consumedBottles + numBottles;
     }
 };
