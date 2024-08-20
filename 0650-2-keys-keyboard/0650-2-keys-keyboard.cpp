7a2e0b14d2c2ad09c38e2f68c5e985af3d9648b5
Omar El-Hariry <48570458+OmarHariry@users.noreply.github.com>
2024-08-20 21:54:02 +0300

    Time: 14 ms (38.29%), Space: 7.5 MB (56.27%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    unordered_map<string, int>memo;
+    int solve(int n, int clipboard, int window) {
+        if (n == window)
+            return 0;
+        if (window > n)
+            return INT_MAX / 2;
+        
+        // string key = to_string(clipboard) + ',' + to_string(window);
+        //     if (memo.find(key) != memo.end())
+        //         return memo[key];
+        int paste = 1 + solve(n, clipboard, window+clipboard);
+        int copyAndPaste =  2 + solve(n, window, window * 2);
+        return min(paste, copyAndPaste);
+        // return memo[key] = min(paste, copyAndPaste);
+    }
+    int minSteps(int n) {
+        if (n == 1)
+            return 0;
+        return 1 + solve(n, 1, 1);
+        // return solve(n, 0, 1);
+    }
+};
